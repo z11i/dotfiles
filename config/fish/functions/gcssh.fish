5663194e62d0b2ed67a0ebc3c158e6e966cb5050
z11i <z11imdk@gmail.com>
2020-11-30 14:53:41 +0800

    update

--- config/fish/functions/gcssh.fish
+++ config/fish/functions/gcssh.fish
@@ -1,6 +1,5 @@
 function gcssh
     set --local user (set -q GC_USER && echo $GC_USER || echo $USER)
     set --local ip (gcloud compute instances list | fzf | awk '{print $4}')
-        timeout 3 ssh $user@$ip ||
-            echo "ssh $user@$ip"; echo "failed, perhaps VPN needed?"
+    nc -w 1 $ip 22 && ssh $user@$ip || echo "ssh $user@$ip failed, perhaps VPN needed?"
 end
