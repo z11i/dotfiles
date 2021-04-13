ff08ddf16e0748cd1e39f2a56120f3dcc65e2376
z11i <z11imdk@gmail.com>
2021-04-13 18:37:03 +0800

    update

--- config/fish/alias.fish
+++ config/fish/alias.fish
@@ -1,9 +1,10 @@
+alias nsh='nix-shell --command fish'
 alias code='/usr/local/bin/code-insiders'
 alias clean="clear && printf '\e[3J'"
 alias g='git'
+alias lc="awk 'END{print NR}'"
 alias tm='tmux new -A -s tmux'
 alias vim='nvim'
-alias lc="awk 'END{print NR}'"
 aliasset yarn yarnpkg
 aliasset l exa
 aliasset k kubectl
