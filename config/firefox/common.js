fb8d15afc4a00b5ed4633a30115a0f23031aa96e
z11i <z11imdk@gmail.com>
2021-04-14 20:43:02 +0800

    update

---
+++ config/firefox/common.js
@@ -0,0 +1,9 @@
+/**
+ * First of all, `devtools.chrome.enabled` must be set to `true` for browser console to work.
+ * See https://superuser.com/a/1609893/1042635 for more details.
+ */
+Services.prefs.setBoolPref('browser.search.suggest.enabled', false);
+Services.prefs.setBoolPref('browser.ctrlTab.sortByRecentlyUsed', true);
+Services.prefs.setStringPref('devtools.theme', 'dark');
+Services.prefs.setBoolPref('extensions.pocket.enabled', false);
+Services.prefs.setBoolPref('findbar.highlightAll', true);
