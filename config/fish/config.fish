042d6768cc542bb747721d2159d0db57833183c0
z11i <z11imdk@gmail.com>
2021-06-09 14:16:38 +0800

    fisher

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -6,6 +6,7 @@ if not functions -q fisher
     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
     fish -c fisher
+    fisher install < $__fish_config_dir/fish_plugins
 end
 
 # Source secret if it exists
