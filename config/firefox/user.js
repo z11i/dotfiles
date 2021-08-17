f829540d03b11df385e9b40f246efa9e9736eff5
Elliot Zhang <elliot@kaedwen.local>
2021-08-17 23:21:11 +0800

    mac apps

---
+++ config/firefox/user.js
@@ -0,0 +1,18 @@
+// ************ theme ************ //
+user_pref('devtools.theme', 'dark');
+user_pref('toolkit.legacyUserProfileCustomizations.stylesheets', true);
+// css blur filter - 88 above
+user_pref("layout.css.backdrop-filter.enabled", true);
+// fill SVG color
+user_pref("svg.context-properties.content.enabled", true);
+
+// ************ browsing ************ //
+user_pref('findbar.highlightAll', true);
+
+// ************ urlbar ************ //
+user_pref('browser.search.suggest.enabled', false);
+user_pref("browser.urlbar.suggest.calculator", true);
+user_pref("browser.urlbar.unitConversion.enabled", true);
+
+// ************ extensions ************ //
+user_pref('extensions.pocket.enabled', false);
