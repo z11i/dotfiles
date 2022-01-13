b63d1961a065316c5868018e12a5970186f6ec64
z11i <z11imdk@gmail.com>
2022-01-13 15:15:37 +0800

    vim: trouble

--- config/nvim/lua/z11i/packer.lua
+++ config/nvim/lua/z11i/packer.lua
@@ -183,8 +183,21 @@ return require('packer').startup(function(use)
         config = function() require('lsp_extensions').inlay_hints{} end,
         ft = {'rust'},
         requires = {{'neovim/nvim-lspconfig', opt = true}}}
+    use {
+        "folke/trouble.nvim",
+        requires = {"kyazdani42/nvim-web-devicons"},
+        config = function()
+            require("trouble").setup {}
+            vim.api.nvim_set_keymap("n", "<leader>xx", "<cmd>Trouble<cr>", {silent = true, noremap = true})
+            vim.api.nvim_set_keymap("n", "<leader>xw", "<cmd>Trouble workspace_diagnostics<cr>", {silent = true, noremap = true})
+            vim.api.nvim_set_keymap("n", "<leader>xd", "<cmd>Trouble document_diagnostics<cr>", {silent = true, noremap = true})
+            vim.api.nvim_set_keymap("n", "<leader>xl", "<cmd>Trouble loclist<cr>", {silent = true, noremap = true})
+            vim.api.nvim_set_keymap("n", "<leader>xq", "<cmd>Trouble quickfix<cr>", {silent = true, noremap = true})
+            vim.api.nvim_set_keymap("n", "gR", "<cmd>Trouble lsp_references<cr>", {silent = true, noremap = true})
+        end
+   }
     --}}}
-    
+
     ---- Git integration {{{
     -- Enable git changes to be shown in sign column
     use { 'tpope/vim-fugitive' }
