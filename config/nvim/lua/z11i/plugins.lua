396e9cf8061927d3a1e4d8b9b079c85beab21034
z11i <z11imdk@gmail.com>
2022-01-24 11:08:21 +0800

    update

--- config/nvim/lua/z11i/plugins.lua
+++ config/nvim/lua/z11i/plugins.lua
@@ -63,6 +63,9 @@ return require('packer').startup(function(use)
                             flip_columns = 160,
                             flip_lines = 10,
                         },
+                        vertical = {
+                            height = 0.7,
+                        },
                         preview_cutoff = 10,
                     },
                     file_ignore_patterns = { 'tags' },
@@ -269,9 +272,49 @@ return require('packer').startup(function(use)
     use {
         'ray-x/go.nvim',
         ft = {'go', 'gomod'},
+        disable = true,
         requires = {
             {'neovim/nvim-lspconfig'},
+            {'mfussenegger/nvim-dap'},
+            {'rcarriga/nvim-dap-ui'},
+            {'theHamsta/nvim-dap-virtual-text'},
         },
+		config = function()
+            vim.opt.expandtab = false
+            vim.opt.shiftwidth=4
+            vim.opt.softtabstop=4
+            vim.opt.tabstop=4
+
+            vim.cmd [[autocmd BufWritePre *.go :lua require('go.format').goimport()]]
+
+            require('go').setup({
+                goimport = 'gopls', -- if set to 'gopls' will use golsp format
+                gofmt = 'gopls', -- if set to gopls will use golsp format
+                max_line_len = 120,
+                tag_transform = false,
+                test_dir = '',
+                comment_placeholder = '<>',
+                lsp_cfg = false, -- false: use your own lspconfig
+                lsp_gofumpt = true, -- true: set default gofmt in gopls format to gofumpt
+                lsp_on_attach = false, -- use on_attach from go.nvim
+                lsp_codelens = true,
+                dap_debug = true,
+            })
+        end,
+    }
+    -- Go development plugin for Vim
+    use {
+        'fatih/vim-go',
+        disable = true,
+    }
+    use {
+        'crispgm/nvim-go',
+        disable = true,
+        requires = {
+            'nvim-lua/plenary.nvim',
+            'nvim-lua/popup.nvim',
+            'neovim/nvim-lspconfig',
+        }
     }
     -- Battery-included rust lsp setup
     use {
