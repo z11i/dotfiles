ff2543dbef4449f00b52d2f59d4581e03cf24405
Elliot Zhang <ezhang@abnormalsecurity.com>
2023-01-06 12:17:33 +0800

    update

--- config/fish/functions/gh_watch_run.fish
+++ config/fish/functions/gh_watch_run.fish
@@ -1,3 +1,7 @@
-function gh_watch_run
-	gh run watch (gh run list --workflow=(ls .github/workflows | grep -E 'ya?ml' | fzf) --json='headBranch,databaseId,number' | jq '[.[] | select(.headBranch == "'(git branch-name)'")] | max_by(.number) | .databaseId')
+function gh_watch_run -a filterstr
+	set -f filter 'fzf'
+	if test -n "$filterstr"
+		set -f filter grep
+	end
+	gh run watch (gh run list --workflow=(ls .github/workflows | grep -E 'ya?ml' | $filter $filterstr) --json='headBranch,databaseId,number' | jq '[.[] | select(.headBranch == "'(git branch-name)'")] | max_by(.number) | .databaseId')
 end
--- config/nvim/lua/plugin/autopairs.lua
+++ config/nvim/lua/plugin/autopairs.lua
@@ -3,6 +3,8 @@ local Rule = require("nvim-autopairs.rule")
 npairs.setup({})
 -- you need setup cmp first put this after cmp.setup()
 -- setup nvim-cmp
-local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-local cmp = require('cmp')
-cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done({  map_char = { tex = '' } }))
+local ok, cmp = pcall(require, "cmp")
+if ok then
+	local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+	cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done({  map_char = { tex = '' } }))
+end
