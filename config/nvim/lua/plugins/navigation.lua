ad5a68608fd6df5a084e0cc871b835054e69f806
Elliot Zhang <ezhang@abnormalsecurity.com>
2023-01-11 21:00:39 +0800

    neovim: leap options

---
+++ config/nvim/lua/plugins/navigation.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "ggandor/leap.nvim",
+    config = function()
+      require("leap").setup({
+        case_sensitive = false,
+      })
+      vim.keymap.set("n", "\\", function()
+        require("leap").leap({
+          target_windows = vim.tbl_filter(function(win)
+            return vim.api.nvim_win_get_config(win).focusable
+          end, vim.api.nvim_tabpage_list_wins(0)),
+        })
+      end)
+      require("flit").setup({}) -- f/t motions
+    end,
+  },
+}
