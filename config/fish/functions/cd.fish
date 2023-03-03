27f3b2362f63e7b463ed773b7ef22eee7854aada
Elliot Zhang <ezhang@abnormalsecurity.com>
2023-03-03 18:36:10 +0800

    fish: do not ls in ~

--- config/fish/functions/cd.fish
+++ config/fish/functions/cd.fish
@@ -1,6 +1,6 @@
 # https://stackoverflow.com/questions/46076344/autols-for-fish-shell
 function __autols_hook --description "Auto ls" --on-event fish_prompt
-  if test "$__autols_last" != (pwd)
+  if test "$__autols_last" != (pwd) && test (pwd) != $HOME
     set -f ls_ignores '-I .DS_Store -I .localized -I .git -I .CFUserTextEncoding'
     set -f exa_ignores '-I \'.DS_Store|.localized|.git|.CFUserTextEncoding\''
     set -f LS "ls -A $ls_ignores"
