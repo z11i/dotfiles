fee83c8bcd1bb2866756e9f5610c7d462d816e2b
Elliot Zhang <z11imdk@gmail.com>
2023-03-07 14:26:27 +0800

    neovim: tweak notify window

--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -1,4 +1,8 @@
 return {
   { "folke/tokyonight.nvim", opts = { style = "night" } },
   { "LazyVim/LazyVim", opts = { colorscheme = "tokyonight" } },
+  { "rcarriga/nvim-notify", opts = {
+    render = "compact",
+    top_down = false,
+  } },
 }
