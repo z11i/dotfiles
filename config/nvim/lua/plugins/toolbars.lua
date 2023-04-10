07fb45c4e0101c186b73379caf144cfde36358cf
Elliot Zhang <z11imdk@gmail.com>
2023-04-10 20:16:45 +0800

    neovim: remove branch in lualine

--- config/nvim/lua/plugins/toolbars.lua
+++ config/nvim/lua/plugins/toolbars.lua
@@ -1,3 +1,10 @@
 return {
   { "kevinhwang91/nvim-bqf", ft = "qf", dependencies = { "junegunn/fzf" } },
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    opts = function(_, opts)
+      opts.sections.lualine_b = {}
+    end,
+  },
 }
