b8281732e5a23cbff9e951c46e312978c44f6b54
Elliot Zhang <z11imdk@gmail.com>
2023-05-12 22:53:54 +0800

    fish: colors

--- config/fish/functions/fish_toggle_color.fish
+++ config/fish/functions/fish_toggle_color.fish
@@ -1,17 +1,14 @@
 function fish_toggle_color
     if test (count $argv) -lt 1
-        if test $FISH_COLORSCHEME = 'light'
-            set -U FISH_COLORSCHEME 'dark'
+        if test $FISH_COLORSCHEME = light
+            set -U FISH_COLORSCHEME dark
             echo 'using dark colorscheme'
-            cp -f ~/.config/fish/colors_dark.fish ~/.config/fish/colors.fish
+            yes | fish_config theme save 'Catppuccin Frappe'
         else
-            set -U FISH_COLORSCHEME 'light'
+            set -U FISH_COLORSCHEME light
             echo 'using light colorscheme'
-            cp -f ~/.config/fish/colors_light.fish ~/.config/fish/colors.fish
+            yes | fish_config theme save 'Catppuccin Latte'
         end
-    else if test $argv = 'light'; or test $argv = 'dark'
-        set -U FISH_COLORSCHEME $argv
-        cp -f ~/.config/fish/colors_$argv.fish ~/.config/fish/colors.fish
     end
 
     sourceadd ~/.config/fish/colors.fish
