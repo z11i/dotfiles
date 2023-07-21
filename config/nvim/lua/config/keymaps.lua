47cc0d34e69d1abe333186f3f2f2af55db5855b3
Elliot Zhang <z11imdk@gmail.com>
2023-07-21 14:58:24 +0800

    neovim: tweak keymap

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -2,6 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 -- vim.keymap.set({ "n", "v" }, ";", ":")
+vim.keymap.set({ "n", "v" }, "\\\\", ":", { desc = "Command line" })
 -- vim.keymap.set({ "n", "v" }, ":", ";")
 
 -- toggle background
