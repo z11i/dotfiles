89387d9566d9a8dcad1dcbffe4407cc4ccd0b5ec
z11i <z11imdk@gmail.com>
2023-12-31 22:29:31 +0800

    neovim: add hawtkeys

--- config/nvim/lua/plugins/neovim.lua
+++ config/nvim/lua/plugins/neovim.lua
@@ -8,6 +8,15 @@ return {
     config = true,
     -- or pass configuration with
     -- opts = {  }
-    lazy = true,
+    lazy = false,
+    priority = 1001,
+  },
+  {
+    "tris203/hawtkeys.nvim",
+    enabled = false, -- dvorak is not yet supported: https://github.com/tris203/hawtkeys.nvim/issues/45
+    opts = {
+      keyboardLayout = "dvorak",
+    },
+    cmd = { "Hawtkeys", "HawtkeysAll", "HawtkeysDupes" },
   },
 }
