09b295721b9879b63e2b0ba184a5346b176349a9
Elliot Zhang <z11imdk@gmail.com>
2024-03-19 11:50:59 +0800

    fish: improve aws in shell

--- config/fish/functions/awsp.fish
+++ config/fish/functions/awsp.fish
@@ -1,3 +1,3 @@
 function awsp
-    set -gx AWS_PROFILE (awk '/\[profile/ {print substr($2, 1, length($2)-1)}' ~/.aws/config | fzf)
+    set -gx AWS_PROFILE (awk '/^\[profile/ {print substr($2, 1, length($2)-1)}' ~/.aws/config | fzf)
 end
