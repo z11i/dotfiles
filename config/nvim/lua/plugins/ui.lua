e7357a008f600f27660bd7ddc2b42e3adb4ace85
z11i <z11imdk@gmail.com>
2024-04-04 23:23:58 +0800

    neovim: borderless telescope

--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -55,8 +55,38 @@ return {
     opts = {
       day_brightness = 0.15,
       lualine_bold = true,
-      on_highlights = function(highlights, colors)
-        highlights.MatchParen = { bg = colors.fg_dark }
+      on_highlights = function(hl, c)
+        hl.MatchParen = { bg = c.fg_dark }
+
+        -- borderless telescope
+        local prompt = "#2d3149"
+        hl.TelescopeNormal = {
+          bg = c.bg_dark,
+          fg = c.fg_dark,
+        }
+        hl.TelescopeBorder = {
+          bg = c.bg_dark,
+          fg = c.bg_dark,
+        }
+        hl.TelescopePromptNormal = {
+          bg = prompt,
+        }
+        hl.TelescopePromptBorder = {
+          bg = prompt,
+          fg = prompt,
+        }
+        hl.TelescopePromptTitle = {
+          bg = prompt,
+          fg = prompt,
+        }
+        hl.TelescopePreviewTitle = {
+          bg = c.bg_dark,
+          fg = c.bg_dark,
+        }
+        hl.TelescopeResultsTitle = {
+          bg = c.bg_dark,
+          fg = c.bg_dark,
+        }
       end,
     },
   },
