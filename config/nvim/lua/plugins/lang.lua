b4b4e1b9d5d4ce8b036fdb1c6d3891adaf4b5a4d
Elliot Zhang <z11imdk@gmail.com>
2024-05-20 17:01:39 +0800

    neovim: remove luasnip

--- config/nvim/lua/plugins/lang.lua
+++ config/nvim/lua/plugins/lang.lua
@@ -3,11 +3,11 @@ return {
     "simrat39/rust-tools.nvim",
     keys = { { "<F5>rr", "<cmd>RustRunnables<cr>", desc = "RustRunnables" } },
   },
-  {
-    -- LuaSnip has been hanging when fetching.
-    -- https://github.com/L3MON4D3/LuaSnip/issues/1071
-    "L3MON4D3/LuaSnip",
-    version = "v2.*",
-    build = "make install_jsregexp",
-  },
+  -- {
+  --   -- LuaSnip has been hanging when fetching.
+  --   -- https://github.com/L3MON4D3/LuaSnip/issues/1071
+  --   "L3MON4D3/LuaSnip",
+  --   version = "v2.*",
+  --   build = "make install_jsregexp",
+  -- },
 }
