09dcac9b14e7ac98fa272212d30eb0bdef3b7313
Elliot Zhang <z11imdk@gmail.com>
2024-08-12 17:22:49 +0800

    nix: use home-manager

---
+++ home-manager/home.nix
@@ -0,0 +1,112 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "z11i";
+  home.homeDirectory = "/Users/z11i";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "24.05"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = [
+    # # Adds the 'hello' command to your environment. It prints a friendly
+    # # "Hello, world!" when run.
+    # pkgs.hello
+
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+    pkgs.bat
+    pkgs.bit
+    pkgs.cacert
+    pkgs.coreutils
+    pkgs.du-dust
+    pkgs.entr
+    pkgs.exercism
+    pkgs.eza
+    pkgs.fd
+    pkgs.findutils
+    pkgs.fzf
+    pkgs.gh
+    pkgs.gitAndTools.delta
+    pkgs.gitAndTools.difftastic
+    pkgs.gitAndTools.gitFull
+    pkgs.gnugrep
+    pkgs.gnupg
+    pkgs.gnused
+    pkgs.htop
+    pkgs.jq
+    pkgs.lazygit
+    pkgs.libressl
+    pkgs.neovim
+    pkgs.nix
+    pkgs.pgcli
+    pkgs.ripgrep
+    pkgs.skim
+    pkgs.socat
+    pkgs.starship
+    pkgs.tmux
+    pkgs.universal-ctags
+    pkgs.unixtools.watch
+    pkgs.vivid
+    pkgs.wget
+    pkgs.yq-go
+    pkgs.zoxide
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. These will be explicitly sourced when using a
+  # shell provided by Home Manager. If you don't want to manage your shell
+  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+  # located at either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/z11i/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
