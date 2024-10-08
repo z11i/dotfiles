9bb66ea540dccbea4b4dbf7a6ab9672f6c9adcf1
Elliot Zhang <z11imdk@gmail.com>
2024-10-08 15:22:58 +0800

    chore: lazyvim

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -137,7 +137,7 @@ return {
 	bold_brightens_ansi_colors = true,
 	enable_scroll_bar = true,
 	font = wezterm.font("Berkeley Mono Variable"),
-	font_size = 15.4,
+	font_size = 16,
 	freetype_load_target = "Light",
 	front_end = "OpenGL",
 	keys = keys,
